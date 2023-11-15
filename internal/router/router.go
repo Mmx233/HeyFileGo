01f0d71b08f31c12709e13a7422ff84a25a25db9
Mmx233 <admin@mmxblog.com>
2023-11-15 19:58:16 +0800

    feat: api 路由

---
+++ internal/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	api "github.com/Mmx233/HeyFileGo/v2/internal/api/router"
+	"github.com/gin-gonic/gin"
+)
+
+var E *gin.Engine
+
+func init() {
+	gin.SetMode(gin.ReleaseMode)
+	E = gin.New()
+	E.Use(gin.Recovery())
+
+	api.Init(E.Group("api"))
+}
