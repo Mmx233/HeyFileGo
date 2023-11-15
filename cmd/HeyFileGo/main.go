56dd285c7d25d002fe0e1f3f46cf2ad831324bce
Mmx233 <admin@mmxblog.com>
2023-11-15 21:47:28 +0800

    feat: 完成网卡选择部分重写

--- cmd/HeyFileGo/main.go
+++ cmd/HeyFileGo/main.go
@@ -3,45 +3,78 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/Mmx233/HeyFileGo/v2/internal/api/controllers"
 	"github.com/Mmx233/HeyFileGo/v2/internal/config"
 	"github.com/Mmx233/HeyFileGo/v2/internal/router"
 	"github.com/Mmx233/HeyFileGo/v2/pkg/cert"
+	"github.com/Mmx233/HeyFileGo/v2/pkg/netInterface"
 	"log"
 	"net"
 	"net/http"
 )
 
+func apiServer(listener net.Listener) {
+	var err error
+	engine := router.Init(config.Mode)
+	if config.Commands.Ssl {
+		var certificate tls.Certificate
+		certificate, err = cert.Gen()
+		if err != nil {
+			panic(err)
+		}
+		srv := &http.Server{
+			Handler: engine,
+			TLSConfig: &tls.Config{
+				Certificates: []tls.Certificate{certificate},
+			},
+		}
+		err = srv.ServeTLS(listener, "", "")
+	} else {
+		err = http.Serve(listener, engine)
+	}
+	if err != nil {
+		log.Fatalln("error: 启动 http 服务失败：", err)
+	}
+}
+
 func main() {
-	listener, e := net.Listen("tcp", ":"+fmt.Sprint(config.Commands.Port))
-	if e != nil {
-		log.Fatalln("error: 监听失败：", e)
+	listener, err := net.Listen("tcp", ":"+fmt.Sprint(config.Commands.Port))
+	if err != nil {
+		log.Fatalln("error: 监听失败：", err)
 	}
 
-	go func() {
-		var err error
-		engine := router.Init(config.Mode)
-		if config.Commands.Ssl {
-			var certificate tls.Certificate
-			certificate, err = cert.Gen()
-			if err != nil {
-				panic(err)
-			}
-			srv := &http.Server{
-				Handler: engine,
-				TLSConfig: &tls.Config{
-					Certificates: []tls.Certificate{certificate},
-				},
-			}
-			err = srv.ServeTLS(listener, "", "")
-		} else {
-			err = http.Serve(listener, engine)
-		}
-		if err != nil {
-			log.Fatalln("error: 启动 http 服务失败：", err)
-		}
-	}()
+	go apiServer(listener)
 
-	controllers.SelectEthToQr(fmt.Sprint(listener.Addr().(*net.TCPAddr).Port))
+	ethList, err := netInterface.Load()
+	if err != nil {
+		log.Println("获取网卡信息失败:", err)
+	} else {
+		printer := netInterface.NewPrinter(config.Commands.Ssl, fmt.Sprint(listener.Addr().(*net.TCPAddr).Port))
+		switch len(ethList) {
+		case 0:
+			fmt.Println("没有找到可用网卡！")
+		case 1:
+			ethUrl := printer.EthUrl(ethList[0])
+			printer.Url(ethUrl)
+			printer.Qr(ethUrl)
+		case 2:
+			ethUrlList := printer.EthSelect(ethList)
+			for {
+				fmt.Printf("选择网卡（序号）：\n")
+				var n int
+				_, err := fmt.Scanln(&n)
+				if err != nil {
+					log.Println("读取输入异常：", err)
+					continue
+				}
+				if len(ethList) <= n || n < 0 {
+					log.Println("warning: 序号不正确：", err)
+					continue
+				}
+				ethUrl := ethUrlList[n]
+				printer.Url(ethUrl)
+				printer.Qr(ethUrl)
+			}
+		}
+	}
 	select {}
 }
--- pkg/netInterface/print.go
+++ pkg/netInterface/print.go
@@ -24,10 +24,14 @@ type Printer struct {
 	port   string
 }
 
+func (p Printer) EthUrl(eth Eth) *url.URL {
+	return eth.Url(p.scheme, p.port)
+}
+
 func (p Printer) EthSelect(list []Eth) []*url.URL {
 	urlList := make([]*url.URL, len(list))
 	for i, eth := range list {
-		ethUrl := eth.Url(p.scheme, p.port)
+		ethUrl := p.EthUrl(eth)
 		urlList[i] = ethUrl
 		fmt.Println(i, fmt.Sprintf("%s（%s）", eth.Name, ethUrl))
 	}
@@ -36,4 +40,8 @@ func (p Printer) EthSelect(list []Eth) []*url.URL {
 
 func (p Printer) Qr(ethUrl *url.URL) {
 	qrcodeTerminal.New().Get(ethUrl).Print()
+}
+
+func (p Printer) Url(addr *url.URL) {
+	fmt.Println("URL:", addr)
 }
