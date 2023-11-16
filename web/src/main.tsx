44e4be26697002a9bb10423998b793f20783d45f
Mmx233 <admin@mmxblog.com>
2023-11-16 12:48:41 +0800

    feat: 上传页拖拽区域

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={createTheme({ palette: { mode: "dark" } })}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
---
+++ web/src/pages/Upload/index.tsx
@@ -0,0 +1,38 @@
+import { FC, useState } from "react";
+import { Stack, Typography, Paper, Fade } from "@mui/material";
+import { UploadFile } from "@mui/icons-material";
+
+export const Upload: FC = () => {
+  const [dragActive, setDragActive] = useState(false);
+
+  return (
+    <Stack>
+      <Stack
+        width={"100%"}
+        alignItems={"center"}
+        py={6}
+        component={Paper}
+        elevation={1}
+        onDrop={(e) => {
+          e.preventDefault();
+          // handler
+          setDragActive(false)
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+          setDragActive(true);
+        }}
+        onDragLeave={() => setDragActive(false)}
+        sx={{
+          transition: "border-color ease-out .1s",
+          borderColor: dragActive ? "info.main" : "transparent",
+          borderStyle: "dotted",
+        }}
+      >
+        <UploadFile color={"primary"} sx={{ fontSize: "4rem" }} />
+        <Typography mt={1.5}>点击上传或将文件拖拽到此区域</Typography>
+      </Stack>
+    </Stack>
+  );
+};
+export default Upload;
