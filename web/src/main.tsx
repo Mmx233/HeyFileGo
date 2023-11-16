5fe623a931961856772fed357c7a2e0b04eb246e
Mmx233 <admin@mmxblog.com>
2023-11-16 17:28:04 +0800

    build: 安装 toast

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { Toaster } from "react-hot-toast";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -10,6 +11,15 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={createTheme({ palette: { mode: "dark" } })}>
       <CssBaseline />
+      <Toaster
+        toastOptions={{
+          style: {
+            borderRadius: "20px",
+            background: "#2f2f2f",
+            color: "#fff",
+          },
+        }}
+      />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
