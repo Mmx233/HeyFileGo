8a00467eeb04cf2f964f718bac53c89e1a19b79b
Mmx233 <admin@mmxblog.com>
2023-11-18 17:29:55 +0800

    style: dir 模式独立 File 组件

---
+++ web/src/pages/Dir/File.tsx
@@ -0,0 +1,33 @@
+import { FC, useRef } from "react";
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { InsertDriveFile } from "@mui/icons-material";
+import { sizeFmt } from "@/utils/fmt.ts";
+
+interface Props {
+  name: string;
+  path: string;
+  size?: number;
+}
+
+export const File: FC<Props> = ({ path, name, size=0 }) => {
+  const downloaderRef = useRef<HTMLAnchorElement | null>(null);
+
+  return (
+    <ListItemButton onClick={() => downloaderRef.current?.click()}>
+      <ListItemIcon>
+        <InsertDriveFile />
+      </ListItemIcon>
+      <ListItemText
+        primary={name}
+        secondary={size != 0 ? sizeFmt(size) : undefined}
+      />
+      <a
+        ref={downloaderRef}
+        style={{ display: "none" }}
+        href={`/api/dir/file?${encodeURI(path + "/" + name)}`}
+        download={name}
+      />
+    </ListItemButton>
+  );
+};
+export default File;
--- web/src/pages/Dir/FolderItem.tsx
+++ web/src/pages/Dir/FolderItem.tsx
@@ -1,9 +1,7 @@
-import { FC, useRef } from "react";
-import { sizeFmt } from "@/utils/fmt.ts";
+import { FC } from "react";
 
 import Folder from "./Folder.tsx";
-import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { InsertDriveFile } from "@mui/icons-material";
+import File from "./File.tsx";
 
 interface Props extends Dir.Info {
   path: string;
@@ -12,26 +10,8 @@ interface Props extends Dir.Info {
 const FolderItem: FC<Props> = ({ path, name, is_dir, size = 0 }) => {
   if (is_dir) {
     return <Folder path={path} name={name} />;
+  } else {
+    return <File path={path} name={name} size={size} />;
   }
-
-  const downloaderRef = useRef<HTMLAnchorElement | null>(null);
-
-  return (
-    <ListItemButton onClick={() => downloaderRef.current?.click()}>
-      <ListItemIcon>
-        <InsertDriveFile />
-      </ListItemIcon>
-      <ListItemText
-        primary={name}
-        secondary={size != 0 ? sizeFmt(size) : undefined}
-      />
-      <a
-        ref={downloaderRef}
-        style={{ display: "none" }}
-        href={`/api/dir/file?${encodeURI(path + "/" + name)}`}
-        download={name}
-      />
-    </ListItemButton>
-  );
 };
 export default FolderItem;
