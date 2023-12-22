0dd771cb6894936fe524fcabec0a2e4107cc08ca
Mmx233 <admin@mmxblog.com>
2023-12-22 20:20:38 +0800

    build: 加速前端构建

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,7 +6,18 @@ const __dirname = resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        [
+          "babel-plugin-direct-import",
+          {
+            modules: ["@mui/material", "@mui/icons-material", "@mui/lab"],
+          },
+        ],
+      ],
+    },
+  })],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
