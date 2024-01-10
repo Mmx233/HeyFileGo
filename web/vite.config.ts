c32bbacb1a54df5e6657f8cf8794548e8008a435
Mmx233 <admin@mmxblog.com>
2024-01-11 00:25:10 +0800

    build: enable frontend legacy support

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {resolve} from "path";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 const __dirname = resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        [
-          "babel-plugin-direct-import",
-          {
-            modules: ["@mui/material", "@mui/icons-material", "@mui/lab"],
-          },
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-direct-import",
+            {
+              modules: ["@mui/material", "@mui/icons-material", "@mui/lab"],
+            },
+          ],
         ],
-      ],
-    },
-  })],
+      },
+    }),
+    legacy(),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
-    }
-  }
-})
+    },
+  },
+});
