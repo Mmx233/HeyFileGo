43d013d9aacc4eaaf67001bd87ed371257413e6f
Mmx <green.dusk8264@fastmail.com>
2024-04-04 02:24:42 +0800

    build: update build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,4 +3,5 @@ SET /p v=Version:
 cd web
 call pnpm run build
 cd ..
-release ./cmd/HeyFileGo --ldflags="-X main.Version=%v%"
+:: https://github.com/Mmx233/GoReleaseCli
+release ./cmd/HeyFileGo --ldflags="-X main.Version=%v%" -c tar.gz
